fe9d5f214ba29127361cf71f78c5faaf7e831697
athira-kunjumon <38275478+athirakvp@users.noreply.github.com>
2020-10-04 16:40:09 +0530

    Update and rename intstr.dart to datatypes.dart
    a simple pgm to introduce datatypes

---
+++ datatypes.dart
@@ -0,0 +1,14 @@
+void main() {
+  int a =10;
+  double d = 12.5;
+  bool flag=true;
+  String n = "a sample string";
+  //int
+    print('value of a - $a');
+  //double
+    print('value of d - $d');
+  //boolean
+  print("value of boolean is $flag");
+  //string
+    print ("content in string is-$n");
+  }
--- intstr.dart
+++
@@ -1,6 +0,0 @@
-void main() {
-  int a =10;
-  String n = "a sample string";
-    print('value of a - $a');
-    print ("content in string is-$n");
-  }
