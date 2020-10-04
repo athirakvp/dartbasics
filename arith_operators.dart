a8caf621aa551469f03e4fee4e5a4b0561257b63
athira-kunjumon <38275478+athirakvp@users.noreply.github.com>
2020-10-04 17:12:06 +0530

    A simple pgm to explain arithmetic operators

---
+++ arith_operators.dart
@@ -0,0 +1,32 @@
+void main() {
+  int a=10;
+  int b=20;
+  var res;
+  print("Value of a is  $a and b is $b");
+  //addition
+  res= a+b;
+  print("Addition result  $res");
+  //subtraction
+  res= a-b;
+  print("Subtraction result  $res");
+  //multiplication
+  res= a*b;
+  print("Multiplication result  $res");
+  //division
+  res= a/b;
+  print("Division result  $res");
+  //division returning integer result
+  res= a~/b;
+  print("Division returning integer  $res");
+  //modulus
+  res = a%b; 
+  print("Modulus result $res"); 
+  //increment 
+  print("Value of a is  $a");
+  a++; 
+  print("Incrementing a gives $a"); 
+ //decrement 
+  print("Value of b is  $b");
+  b--; 
+  print("Decrementing b gives $b"); 
+}
