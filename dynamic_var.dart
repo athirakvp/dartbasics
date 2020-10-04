597e2ed3cb295ef542d527ad138b812b729b2bdc
athira-kunjumon <38275478+athirakvp@users.noreply.github.com>
2020-10-04 16:50:45 +0530

    dynamic variables in dart

---
+++ dynamic_var.dart
@@ -0,0 +1,17 @@
+//pgm to show dynamic variables in dart
+void main() {
+  var a;
+  print (a);
+  //int
+  a=10;
+  print(a);
+  //double
+  a=10.11;
+  print(a);
+  //boolean
+  a=true;
+  print(a);
+  //string
+  a="hello_world";
+  print(a);
+  }
